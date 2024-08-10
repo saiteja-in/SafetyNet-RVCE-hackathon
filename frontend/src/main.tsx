477c90efdc0daa27987600c25fbf28444dc9cee0
saiteja-in <vurukondasaiteja13@gmail.com>
2024-08-10 15:27:00 +0530

    add redux toolkit for state management

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { store } from "./redux/store.ts";
+import { Provider } from "react-redux";
+import { persistor } from "./redux/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </PersistGate>
+  </React.StrictMode>
+);
---
+++ frontend/src/redux/store.ts
@@ -0,0 +1,19 @@
+import { configureStore,combineReducers } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import persistStore from "redux-persist/es/persistStore";
+import storage from "redux-persist/lib/storage";
+const rootReducer=combineReducers({
+
+})
+const persistConfig={
+    key:"root",
+    storage,
+    version:1,
+}
+const persistedReducer=persistReducer(persistConfig,rootReducer);
+
+export const store=configureStore({
+    reducer:persistedReducer,
+    middleware:(getDefaultMiddleware)=>getDefaultMiddleware({serializableCheck:false}),
+  })
+  export const persistor=persistStore(store);
