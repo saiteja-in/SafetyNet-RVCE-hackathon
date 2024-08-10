3298f1d964798c3b55ba9b7cf2b56df15d52393e
Nikhilesh <nikhileshg02@gmail.com>
2024-08-11 00:31:21 +0530

    google maps adding

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import Header from "./components/Header";
 import Signin from "./pages/Signin";
 import SignUp from "./pages/SignUp";
+import NearestSafeLocs from "./pages/NearestSafeLocs";
+
 const App = () => {
   return (
     <>
@@ -13,6 +15,7 @@ const App = () => {
           <Route path="/" element={<Home/>} />
           <Route path="/sign-in" element={<Signin/>} />
           <Route path="/sign-up" element={<SignUp/>} />
+          <Route path="/safe-loc" element={<NearestSafeLocs/>} />
         </Routes>
       </BrowserRouter>
     </>
